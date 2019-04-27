07c5a28157fe10700e6479df9518f427a8b38429
kubo-hide-kun <hide44041@gmail.com>
2019-04-27 20:51:51 +0900

    ポケベルのUIの作成

--- src/views/Pokebell.vue
+++ src/views/Pokebell.vue
@@ -1,6 +1,18 @@
 <template>
-    <div class="pokebell">
-        ポケベルのページ
+    <div class="external">
+        <div class="inside">
+            <div class="screen">
+                結果の出力
+            </div>
+            <div class="under">
+                <div class="unser_left">
+                    <button>    </button>
+                    <button>    </button>
+                    <button>    </button>
+                </div>
+                <div class="unser_right"></div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,7 +23,42 @@
 </script>
 
 <style scoped>
-    .pokebell {
-
+    .external {
+        background-color:#666;
+        border-radius:1em;
+        width:300px;
+        padding:18px;
+    }
+    .inside {
+        background-color:#333;
+        border-radius:1em;
+        padding:14px;
+    }
+    .screen {
+        color:#fff;
+        background-color:#6E8556;
+        margin:5px;
+        padding:5px;
+        padding-left:15px;
+        padding-right:15px;
+    }
+    .under {
+        display: table;
+        margin-top: 15px;
+        width:100%;
+    }
+    .under_left {
+        display: table-cell;
+        width:55%;
+    }
+    .unser_right {
+        display: table-cell;
+        border-top: groove 10px #FAEF62;
+        width:30%;
+    }
+    button {
+        width: 18%;
+        margin-left: 1%;
+        margin-right: 1%;
     }
 </style>
